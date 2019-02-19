e376c4609cff8f87562519ae4e08ecc7ec2a7065
Matteo <mrtn.matteo@protonmail.com>
2019-02-19 17:46:44 +0100

    day1 part2 complete solution

--- 2017/day1/day1.go
+++ 2017/day1/day1.go
@@ -7,7 +7,7 @@ import "strconv"
 
 func main() {
 	fd, _ := os.Open("./input")
-	var nAnswer uint
+	var nAnswer [2]uint
 	var sBuf = make([]byte, 1, 1)
 	var sValues = make([]uint8, 0, 512)
 
@@ -24,12 +24,16 @@ func main() {
 
 	//compute answer
 	for i, e := range sValues {
-		if e == sValues[(i+1)%len(sValues)] {
-			nAnswer += uint(e)
+		if e == sValues[(i + 1) % len(sValues)] {
+			nAnswer[0] += uint(e)
+		}
+		if e == sValues[(i + (len(sValues) / 2)) % len(sValues)] {
+			nAnswer[1] += uint(e)
 		}
 	}
 
 	//print answer
 	fmt.Printf("There are %v values\n", len(sValues))
-	fmt.Printf("The answer is: %v\n", nAnswer)
+	fmt.Printf("The answer to the first part is: %v\n", nAnswer[0])
+	fmt.Printf("The answer to the second part is: %v\n", nAnswer[1])
 }
