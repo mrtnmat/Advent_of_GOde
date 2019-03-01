e45aa1161b952a3c7dd8bc0e445b3760704814d6
Matteo <mrtn.matteo@protonmail.com>
2019-03-01 09:41:02 +0100

    2018 day5: part1 solution

--- 2018/day05/part1/main.go
+++ 2018/day05/part1/main.go
@@ -1,54 +1,55 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"io/ioutil"
+	"time"
 )
 
 const (
-  slp = time.Duration(1 << 25)
+	slp = time.Duration(1 << 10)
 )
 
 type (
-  polymer []byte
+	polymer []byte
 )
 
 func main() {
-  var p polymer
-  p = []byte(input)
-  //fmt.Printf("%s\n", pc)
-  p = p.react()
-  fmt.Printf("%v monomers left after reacting completely.\n", len(p))
+	var p polymer
+	p, _ = ioutil.ReadFile("./input.txt")
+	p = p[:len(p)-1]
+	p = p.react()
+	fmt.Printf("%v monomers left after reacting completely.\n", len(p))
 }
 
 func (pc polymer) react() polymer {
-  w := 0
-  for i, _ := range pc {
-    pc[w] = pc[i]
-    fmt.Printf("%c", pc[w])
-    time.Sleep(slp)
-    if w > 1 && is_reactive(pc[w], pc[w-1]) {
-      fmt.Print("\010\040\010")
-      time.Sleep(slp)
-      w--
-      fmt.Print("\010\040\010")
-      time.Sleep(slp)
-      w--
-    }
-    w++
-  }
-  fmt.Print("\n")
-  return pc[:w]
+	w := 0
+	for i, _ := range pc {
+		pc[w] = pc[i]
+		fmt.Printf("%c", pc[w])
+		time.Sleep(slp)
+		if w > 1 && is_reactive(pc[w], pc[w-1]) {
+			fmt.Print("\010\040\010")
+			time.Sleep(slp)
+			w--
+			fmt.Print("\010\040\010")
+			time.Sleep(slp)
+			w--
+		}
+		w++
+	}
+	fmt.Print("\n")
+	return pc[:w]
 }
 
 func is_reactive(m1, m2 byte) bool {
-  d := int(m1) - int(m2)
-  if d < 0 {
-    d = -d
-  }
-  if d == 'a' - 'A' {
-    return true
-  } else {
-    return false
-  }
+	d := int(m1) - int(m2)
+	if d < 0 {
+		d = -d
+	}
+	if d == 'a'-'A' {
+		return true
+	} else {
+		return false
+	}
 }
